c4492d443e7e8b71dfcc87fc2f6f74811b3b5152
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-03-02 22:19:31 -0800

    Add update and new actions

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -10,4 +10,34 @@ class QuotesController < Rulers::Controller
   def exception
     raise "It's a bad one!"
   end
+
+  def index
+    quotes = FileModel.all
+    render :index, quotes: quotes
+  end
+
+  def quote_1
+    quote_1 = FileModel.find(1)
+    render :quote, obj: quote_1
+  end
+
+  def new_quote
+    attrs = {
+      "submitter"  => "web user",
+      "quote" => "A picture is worth one k pixels",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, obj: m
+  end
+
+  def update_quote
+    id = 10
+    attrs = {
+      "submitter" =>  "new user"
+    }
+
+    u = FileModel.save(attrs, id)
+    render :quote, obj: u
+  end
 end
